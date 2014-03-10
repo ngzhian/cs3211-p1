8b3aee889e9e8706792d9e09cc7f3f2ddc4f33b3
Jerome Cheng <jcheng31@gmail.com>
2014-03-10 13:36:09 +0800

    Reindent.

--- src/ProcessingUnit.java
+++ src/ProcessingUnit.java
@@ -11,66 +11,67 @@ import java.net.UnknownHostException;
  * to fulfil the requests of the User (Atm).
  */
 public class ProcessingUnit extends Thread {
-  private int account;
-  int withdrawAmount;
-  private boolean complete;
-  private boolean success;
+	private int account;
+	int withdrawAmount;
+	private boolean complete;
+	private boolean success;
 
-  public ProcessingUnit(int account) {
-    this.account = account;
-  }
+	public ProcessingUnit(int account) {
+		this.account = account;
+	}
 
-  public void setWithdrawAmount(int amount) {
-    this.withdrawAmount = amount;
-  }
+	public void setWithdrawAmount(int amount) {
+		this.withdrawAmount = amount;
+	}
 
-  @Override
-  public void run() {
-    complete = false;
-    success = false;
+	@Override
+	public void run() {
+		complete = false;
+		success = false;
 
-    try (Socket socket = new Socket("localhost", Globals.port);
-        BufferedReader inFromServer = new BufferedReader(new InputStreamReader(
-            socket.getInputStream()));
-        PrintWriter outToServer = new PrintWriter(socket.getOutputStream(),
-            true);) {
-      String inputLine;
+		try (Socket socket = new Socket("localhost", Globals.port);
+				BufferedReader inFromServer = new BufferedReader(
+						new InputStreamReader(socket.getInputStream()));
+				PrintWriter outToServer = new PrintWriter(
+						socket.getOutputStream(), true);) {
+			String inputLine;
 
-      String command = "withdraw " + account + " " + withdrawAmount;
-      System.out.println("PU issues command:" + command);
-      outToServer.println(command);
-      while ((inputLine = inFromServer.readLine()) != null) {
-        System.out.println("PU receives feedback:" + inputLine);
-        switch (inputLine) {
-          case "end" :
-            break;
-          case "success" :
-            success = true;
-            break;
-          case "fail" :
-            success = false;
-            break;
-        }
-        // notify the database that ProcessingUnit wishes to end transaction
-        outToServer.println("end");
-        break;
-      }
-    } catch (UnknownHostException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    } catch (IOException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    }
-    complete = true;
-  }
+			String command = "withdraw " + account + " " + withdrawAmount;
+			System.out.println("PU issues command:" + command);
+			outToServer.println(command);
+			while ((inputLine = inFromServer.readLine()) != null) {
+				System.out.println("PU receives feedback:" + inputLine);
+				switch (inputLine) {
+				case "end":
+					break;
+				case "success":
+					success = true;
+					break;
+				case "fail":
+					success = false;
+					break;
+				}
+				// notify the database that ProcessingUnit wishes to end
+				// transaction
+				outToServer.println("end");
+				break;
+			}
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		complete = true;
+	}
 
-  public boolean isCompleted() {
-    return complete;
-  }
+	public boolean isCompleted() {
+		return complete;
+	}
 
-  public boolean hasFailed() {
-    return !success;
-  }
+	public boolean hasFailed() {
+		return !success;
+	}
 
 }
