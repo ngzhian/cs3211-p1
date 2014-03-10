240edc7f913ca2abd64526a19eb852626ba9c21b
Jerome Cheng <jcheng31@gmail.com>
2014-03-10 12:29:04 +0800

    Implement Network class.
    This class provides a unidirectional possibly unreliable network.

--- src/Network.java
+++ src/Network.java
@@ -1,22 +1,63 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-
+import java.net.UnknownHostException;
+import java.util.Random;
 
 public class Network extends Thread {
+	private int receivePortNum;
+	private int sendPortNum;
+
+	private int percentageSuccess;
+	private Random randomGenerator;
+
+	public Network(int receivePort, int sendPort, int percentageSuccess) {
+		this.receivePortNum = receivePort;
+		this.sendPortNum = sendPort;
+		this.percentageSuccess = percentageSuccess;
+		this.randomGenerator = new Random();
+	}
+
 	public void run() {
-		try (
-				ServerSocket receiveFromAtm = new ServerSocket(Globals.atmSendPort);
-				Socket sendToAtm = new Socket("localhost", Globals.atmReceivePort);
+		try (ServerSocket receivePort = new ServerSocket(this.receivePortNum)) {
+			while (true) {
+				Socket receivedFrom = receivePort.accept();
 
-				ServerSocket receiveFromPu = new ServerSocket(Globals.puSendPort);
-				Socket sendToPu = new Socket("localhost", Globals.puReceivePort);
-
-				ServerSocket receiveFromDb = new ServerSocket(Globals.dbSendPort);
-				Socket sendToDb = new Socket("localhost", Globals.dbReceivePort)
-		) {
-			
+				boolean isConnectionSuccessful = randomGenerator.nextInt(100) < this.percentageSuccess;
+				if (!isConnectionSuccessful) {
+					dropConnection(receivedFrom);
+					continue;
+				} else {
+					forwardMessage(receivedFrom);
+				}
+			}
 		} catch (IOException e) {
+
+		}
+	}
+	
+	private void dropConnection(Socket receivedFrom) throws IOException {
+		receivedFrom.close();
+	}
+
+	private void forwardMessage(Socket receivedFrom) throws IOException,
+			UnknownHostException {
+		BufferedReader inFromSender = new BufferedReader(
+				new InputStreamReader(receivedFrom.getInputStream()));
+
+		try (Socket sendTo = new Socket("localhost",
+				this.sendPortNum)) {
+			BufferedWriter outToReceiver = new BufferedWriter(
+					new OutputStreamWriter(sendTo.getOutputStream()));
+
+			String lineFromSender;
+			while ((lineFromSender = inFromSender.readLine()) != null) {
+				outToReceiver.write(lineFromSender);
+			}
 		}
 	}
 }
