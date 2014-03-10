07caad60e2e670310fece0defb185e77f241ba8f
Jerome Cheng <jcheng31@gmail.com>
2014-03-10 19:27:05 +0800

    Rewrite AuthServer.

--- src/AuthServer.java
+++ src/AuthServer.java
@@ -7,45 +7,46 @@ import java.net.Socket;
 
 public class AuthServer extends Thread {
 
-  @Override
-  public void run() {
-    try (ServerSocket serverSocket = new ServerSocket(
-        Globals.authNetworkToAuthServer)) {
-      while (true) {
-        // starts a new thread to handle each client connection
-        Socket client = serverSocket.accept();
-        AuthServerThread authServerThread = new AuthServerThread(client);
-        authServerThread.start();
-      }
-    } catch (IOException e1) {
-      System.out
-          .println("ERROR: Couldn't get input stream in worker. Halting.");
-      return;
-    }
-  }
+	@Override
+	public void run() {
+		try (ServerSocket serverSocket = new ServerSocket(Globals.atmToAuthPort)) {
+			while (true) {
+				// starts a new thread to handle each client connection
+				Socket client = serverSocket.accept();
+				AuthServerThread authServerThread = new AuthServerThread(client);
+				authServerThread.start();
+			}
+		} catch (IOException e1) {
+			System.out
+					.println("ERROR: Couldn't get input stream in worker. Halting.");
+			return;
+		}
+	}
 }
 
 class AuthServerThread extends Thread {
-	private Socket receiveSocket;
+	private Socket client;
 
 	public AuthServerThread(Socket socket) {
-		this.receiveSocket = socket;
+		this.client = socket;
 	}
 
 	@Override
 	public void run() {
-		try (BufferedReader inFromClient = new BufferedReader(new InputStreamReader(receiveSocket.getInputStream()));
-			 Socket sendSocket = new Socket("localhost", Globals.authServerToAuthNetwork);
-			 PrintWriter serverOut = new PrintWriter(sendSocket.getOutputStream(),true);) {
-			
-			boolean isValid = Integer.parseInt(inFromClient.readLine()) < Globals.accountNumber;
-			String message = isValid ? "success" : "invalid account";
+		try (BufferedReader inFromClient = new BufferedReader(
+				new InputStreamReader(client.getInputStream()));
+				PrintWriter serverOut = new PrintWriter(
+						client.getOutputStream(), true);) {
+
+			boolean isTimeout = Globals.randomGenerator.nextInt(100) < Globals.reliability;
+			String message;
+			if (isTimeout) {
+				message = "timeout";
+			} else {
+				boolean isValid = Integer.parseInt(inFromClient.readLine()) < Globals.numberOfAccounts;
+				message = isValid ? "success" : "invalid account";
+			}
 			serverOut.println(message);
-			
-			//Wait until the other side reads everything
-			long wait = System.currentTimeMillis() + 1000;
-			while(System.currentTimeMillis() < wait);
-			
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
