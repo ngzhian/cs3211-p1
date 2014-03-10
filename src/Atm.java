f489098533b918f557e258c5c41b5fd827d67c8e
Jerome Cheng <jcheng31@gmail.com>
2014-03-10 19:35:57 +0800

    Rewrite ATM.

--- src/Atm.java
+++ src/Atm.java
@@ -2,39 +2,33 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Atm extends Thread {
 	Integer account;
 	Integer amount;
-	
+
 	public void login(Integer account) {
 		System.out.println("Logging in...");
 		try (Socket authConnection = new Socket("localhost",
-				Globals.atmToAuthNetwork);
+				Globals.atmToAuthPort);
 				PrintWriter writer = new PrintWriter(
 						authConnection.getOutputStream(), true);
 				BufferedReader reader = new BufferedReader(
-						new InputStreamReader(authConnection.getInputStream()));
-				ServerSocket serverSocket = new ServerSocket(Globals.authNetworkToAtm);) {
+						new InputStreamReader(authConnection.getInputStream()));) {
 			writer.println(account);
 
-			if(reader.readLine().equals("timeout")){
+			String message = reader.readLine();
+			switch (message) {
+			case "timeout":
 				throw new IOException();
-			}
-			
-			Socket receivedFrom = serverSocket.accept();
-	        BufferedReader reply = new BufferedReader(
-					new InputStreamReader(receivedFrom.getInputStream()));
-	        String message = reply.readLine();
-			boolean isSuccess = message.equals("success");
-			if(isSuccess){
+			case "success":
 				this.account = account;
-			} else{
-				login(account);
+				break;
+			default:
+				System.out.println("Account does not exist!");
+				break;
 			}
-			
 		} catch (IOException e) {
 			// Connection failed
 			System.out.println("Login failed.. trying again");
@@ -47,11 +41,17 @@ public class Atm extends Thread {
 	}
 
 	private void withdrawAmount(Integer amount) {
-		try (Socket puConnection = new Socket("localhost",
-				Globals.atmToPuNetwork)) {
-			PrintWriter writer = new PrintWriter(
-					puConnection.getOutputStream(), true);
-			writer.println("withdraw " + this.account + " " + amount.toString());
+		try (Socket puConnection = new Socket("localhost", Globals.atmToPuPort);
+				PrintWriter outToPu = new PrintWriter(
+						puConnection.getOutputStream(), true);
+				BufferedReader inFromPu = new BufferedReader(
+						new InputStreamReader(puConnection.getInputStream()))) {
+
+			outToPu.println("withdraw " + this.account + " "
+					+ amount.toString());
+			
+			String response = inFromPu.readLine();
+			System.out.println(response);
 		} catch (IOException e) {
 			// The connection failed.
 			System.out.println(e.getMessage());
--- src/Globals.java
+++ src/Globals.java
@@ -1,9 +1,12 @@
+import java.util.Random;
 
 public class Globals {
 	static int reliability = 100;
-	static int accountNumber = 10;
-	
+	static int numberOfAccounts = 10;
+
 	static int atmToAuthPort = 1000;
 	static int atmToPuPort = 1001;
 	static int puToDbPort = 1002;
+	
+	static Random randomGenerator = new Random();
 }
