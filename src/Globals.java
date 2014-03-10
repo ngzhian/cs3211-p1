6b2bb8d7ab3f1d288fbe8c9fddfc54461fc0321e
Jerome Cheng <jcheng31@gmail.com>
2014-03-10 19:18:44 +0800

    Wishful thinking reimplementation of Globals and Main.

--- src/Globals.java
+++ src/Globals.java
@@ -1,22 +1,9 @@
 
 public class Globals {
-
-	static int port;
-	
+	static int reliability = 100;
 	static int accountNumber = 10;
 	
-	static int atmToAuthNetwork = 1232;
-	static int authNetworkToAuthServer = 1233;
-	static int authServerToAuthNetwork = 1234;
-	static int authNetworkToAtm = 1235;
-	
-	static int atmToPuNetwork = 1236;
-	static int puNetworkToPu = 1237;
-	static int puToPuNetwork = 1238;
-	static int puNetworkToAtm = 1239;
-
-	static int puToDbNetwork = 1240;
-	static int dbNetworkToDb = 1241;
-	static int dbToDbNetwork = 1242;
-	static int dbNetworkToPu = 1243;
+	static int atmToAuthPort = 1000;
+	static int atmToPuPort = 1001;
+	static int puToDbPort = 1002;
 }
--- src/Main.java
+++ src/Main.java
@@ -1,73 +1,56 @@
 public class Main {
-  public static void main(String[] args) {
-    int reliability = 100;
+	public static void main(String[] args) {
+		if (args.length > 0) {
+			Globals.reliability = Integer.parseInt(args[0]);
+		}
+		setUpDatabase();
 
-    setUpNetwork(reliability);
-    setUpDatabase();
+		ProcessingUnit pu = new ProcessingUnit();
+		pu.start();
 
-    ProcessingUnit pu = new ProcessingUnit();
-    pu.start();
+		AuthServer auth = new AuthServer();
+		auth.start();
 
-    AuthServer auth = new AuthServer();
-    auth.start();
+		runTest();
+	}
 
-    runTest();
-  }
+	private static void runTest() {
+		Atm first = new Atm();
+		Atm second = new Atm();
 
-  private static void runTest() {
-    Atm first = new Atm();
-    Atm second = new Atm();
+		first.login(0);
+		second.login(0);
 
-    first.login(0);
-    second.login(0);
+		Integer withdrawA = 30;
+		Integer withdrawB = 40;
+		int expected = 30;
 
-    Integer withdrawA = 30;
-    Integer withdrawB = 40;
-    int expected = 30;
+		System.out.println("Database has 2 accounts, with 100 each");
+		System.out.println("ATM 1 will withdraw " + withdrawA
+				+ " from account 1");
+		System.out.println("ATM 2 will withdraw " + withdrawB
+				+ " from account 1");
+		first.setWithdrawAmount(withdrawA);
+		second.setWithdrawAmount(withdrawB);
 
-    System.out.println("Database has 2 accounts, with 100 each");
-    System.out.println("ATM 1 will withdraw " + withdrawA + " from account 1");
-    System.out.println("ATM 2 will withdraw " + withdrawB + " from account 1");
-    first.setWithdrawAmount(withdrawA);
-    second.setWithdrawAmount(withdrawB);
+		first.start();
+		second.start();
 
-    first.start();
-    second.start();
+		System.out.println("Final balance should be " + expected);
+		Integer actual = Database.getBalance(0);
+		if (actual == expected) {
+			System.out.println("And is " + expected
+					+ ". This may happen sometimes, run the program again");
+		} else {
+			System.out.println("But is " + actual);
+		}
+		System.out
+				.println("Main exit. If running from eclipse remember to manually stop, else Database will continue running and hog the socket port");
+	}
 
-    System.out.println("Final balance should be " + expected);
-    Integer actual = Database.getBalance(0);
-    if (actual == expected) {
-      System.out.println("And is " + expected
-          + ". This may happen sometimes, run the program again");
-    } else {
-      System.out.println("But is " + actual);
-    }
-    System.out
-        .println("Main exit. If running from eclipse remember to manually stop, else Database will continue running and hog the socket port");
-  }
-
-  private static void setUpDatabase() {
-    Database db = new Database(Globals.dbNetworkToDb, 2);
-    Database.setDefaultBalance();
-    db.start();
-  }
-
-  private static void setUpNetwork(int reliability) {
-    Network atmToAuth = new Network(Globals.atmToAuthNetwork,
-        Globals.authNetworkToAuthServer, reliability);
-    Network authToAtm = new Network(Globals.authServerToAuthNetwork, Globals.authNetworkToAtm, reliability);
-    Network atmToPu = new Network(Globals.atmToPuNetwork, Globals.puNetworkToPu,
-        reliability);
-    Network puToAtm = new Network(Globals.puToPuNetwork, Globals.puNetworkToAtm, reliability);
-    Network puToDb = new Network(Globals.puToDbNetwork, Globals.dbNetworkToDb,
-        reliability);
-    Network dbToPu = new Network(Globals.dbToDbNetwork, Globals.dbNetworkToPu, reliability);
-    
-    atmToAuth.start();
-    authToAtm.start();
-    atmToPu.start();
-    puToAtm.start();
-    puToDb.start();
-    dbToPu.start();
-  }
+	private static void setUpDatabase() {
+		Database db = new Database(Globals.puToDbPort, 2);
+		Database.setDefaultBalance();
+		db.start();
+	}
 }
