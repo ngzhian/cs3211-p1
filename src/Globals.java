42abb15e6fedfa21b8db983f0f6a897dda24c7a3
Jerome Cheng <jcheng31@gmail.com>
2014-03-10 09:55:06 +0800

    Add ports to Globals.

--- src/Globals.java
+++ src/Globals.java
@@ -2,5 +2,13 @@
 public class Globals {
 
 	static int port;
+	
+	static int atmSendPort = 1234;
+	static int atmReceivePort = 1235;
 
+	static int puSendPort = 1236;
+	static int puReceivePort = 1237;
+
+	static int dbSendPort = 1238;
+	static int dbReceivePort = 1239;
 }
