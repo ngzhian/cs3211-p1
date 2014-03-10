6a94200c052932c5c95e7086be012adac5230864
Jerome Cheng <jcheng31@gmail.com>
2014-03-10 12:45:21 +0800

    Add additional ports needed by Processing Unit.

--- src/Globals.java
+++ src/Globals.java
@@ -7,8 +7,10 @@ public class Globals {
 	static int atmReceivePort = 1235;
 
 	static int puSendPort = 1236;
-	static int puReceivePort = 1237;
+	static int puReceiveAtmPort = 1237;
+	static int puReplyAtmPort = 1238;
+	static int puReceiveDbPort = 1239;
 
-	static int dbSendPort = 1238;
-	static int dbReceivePort = 1239;
+	static int dbSendPort = 1240;
+	static int dbReceivePort = 1241;
 }
